045d709e71f1ec5cfc758ec0036a7475c45541e1
MaoZhongSheng <maozhong6336@qq.com>
2020-06-29 17:08:05 +0800

    上午提交

---
+++ day02/src/cn/jy/Test.java
@@ -0,0 +1,14 @@
+package cn.jy;
+
+public class Test {
+public static void main(String[] args) {
+	
+System.out.println("上午提交");
+System.out.println("上午提交");
+System.out.println("上午提交");
+System.out.println("上午提交");
+System.out.println("上午提交");
+System.out.println("上午提交");
+System.out.println("上午提交");
+}
+}
